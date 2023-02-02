dfb5af265268a5b1281608511d3f246d92b6b095
Johnny Miller <johnny@digitalis.io>
2023-02-02 10:24:53 +0100

    project init

---
+++ jar-with-dependencies/src/main/java/io/digitalis/example/velocity/HelloWorld.java
@@ -0,0 +1,10 @@
+package io.digitalis.example.velocity;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println(StringUtils.upperCase("Hello World"));
+    }
+}
