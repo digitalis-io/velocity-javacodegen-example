714b977f5a0f47641cecbcb01481032b501f9312
Johnny Miller <johnny@digitalis.io>
2023-02-02 10:33:54 +0100

    tidying up init

--- jar-with-dependencies/src/main/java/io/digitalis/example/velocity/Main.java
+++ jar-with-dependencies/src/main/java/io/digitalis/example/velocity/Main.java
@@ -2,7 +2,7 @@ package io.digitalis.example.velocity;
 
 import org.apache.commons.lang3.StringUtils;
 
-public class HelloWorld {
+public class Main {
 
     public static void main(String[] args) {
         System.out.println(StringUtils.upperCase("Hello World"));
